e3ba0867082d1346a8ff346215b9dccd74286aa5
Amey <amey23399@gmail.com>
2021-08-11 16:29:39 +0530

    profile page backend added

--- pages/manufacturer/profile.vue
+++ pages/manufacturer/profile.vue
@@ -4,7 +4,7 @@
     <div class="row mb-3">
         <div class="col-lg-4">
             <div class="card mb-3">
-                <div class="card-body text-center shadow"><img class="rounded-circle mb-3 mt-4" src="assets/img/dogs/image2.jpeg" width="160" height="160">
+                <div class="card-body text-center shadow"><img class="rounded-circle mb-3 mt-4" src="/assets/img/dogs/image2.jpeg" width="160" height="160">
                     <div class="mb-3"><button class="btn btn-primary btn-sm" type="button">Change Photo</button></div>
                 </div>
             </div>
@@ -74,23 +74,23 @@
                             <p class="text-primary m-0 fw-bold">User Settings</p>
                         </div>
                         <div class="card-body">
-                            <form>
+                            <form @submit.prevent="saveUserSettings()">
                                 <div class="row">
                                     <div class="col">
-                                        <div class="mb-3"><label class="form-label" for="username"><strong>Username</strong></label><input class="form-control" type="text" id="username" placeholder="user.name" name="username"></div>
+                                        <div class="mb-3"><label class="form-label" for="username"><strong>Company</strong></label><input v-model="userData.username" class="form-control" type="text" id="username" placeholder="Company" name="username"></div>
                                     </div>
                                     <div class="col">
-                                        <div class="mb-3"><label class="form-label" for="email"><strong>Email Address</strong></label><input class="form-control" type="email" id="email" placeholder="user@example.com" name="email"></div>
+                                        <div class="mb-3"><label class="form-label" for="email"><strong>Email Address</strong></label><input v-model="userData.email" class="form-control" type="email" id="email" placeholder="Email" name="email"></div>
                                     </div>
                                 </div>
-                                <div class="row">
+                                <!-- <div class="row">
                                     <div class="col">
                                         <div class="mb-3"><label class="form-label" for="first_name"><strong>First Name</strong></label><input class="form-control" type="text" id="first_name" placeholder="John" name="first_name"></div>
                                     </div>
                                     <div class="col">
                                         <div class="mb-3"><label class="form-label" for="last_name"><strong>Last Name</strong></label><input class="form-control" type="text" id="last_name" placeholder="Doe" name="last_name"></div>
                                     </div>
-                                </div>
+                                </div> -->
                                 <div class="mb-3"><button class="btn btn-primary btn-sm" type="submit">Save Settings</button></div>
                             </form>
                         </div>
@@ -100,20 +100,21 @@
                             <p class="text-primary m-0 fw-bold">Contact Settings</p>
                         </div>
                         <div class="card-body">
-                            <form>
-                                <div class="mb-3"><label class="form-label" for="address"><strong>Address</strong></label><input class="form-control" type="text" id="address" placeholder="Sunset Blvd, 38" name="address"></div>
-                                <div class="row">
+                            <form @submit.prevent="saveContactSettings()">
+                                <div class="mb-3"><label class="form-label" for="address"><strong>Address</strong></label><input v-model="userData.address" class="form-control" type="text" id="address" placeholder="Address" name="address"></div>
+                                <!-- <div class="row">
                                     <div class="col">
                                         <div class="mb-3"><label class="form-label" for="city"><strong>City</strong></label><input class="form-control" type="text" id="city" placeholder="Los Angeles" name="city"></div>
                                     </div>
                                     <div class="col">
                                         <div class="mb-3"><label class="form-label" for="country"><strong>Country</strong></label><input class="form-control" type="text" id="country" placeholder="USA" name="country"></div>
                                     </div>
-                                </div>
-                                <div class="mb-3"></div>
+                                </div> -->
+                                <!-- <div class="mb-3"></div> -->
+                                <div class="mb-3"><label class="form-label" for="contact"><strong>Contact Number</strong></label><input v-model="userData.phone" class="form-control" type="tel" id="contact" placeholder="Mobile No." name="contact"></div>
+                                <!-- <div class="mb-3"><label class="form-label" for="address"><strong>Pin Code</strong></label><input type="text" id="address-2" placeholder="Sunset Blvd, 38" name="address"></div> -->
+                                <div class="mb-3"><button class="btn btn-primary btn-sm" type="submit">Save Settings</button></div>
                             </form>
-                            <div class="mb-3"><label class="form-label" for="address"><strong>Contact Number</strong></label><input type="tel" id="address-1" placeholder="Sunset Blvd, 38" name="address"></div>
-                            <div class="mb-3"><label class="form-label" for="address"><strong>Pin Code</strong></label><input type="text" id="address-2" placeholder="Sunset Blvd, 38" name="address"></div><button class="btn btn-primary btn-sm" type="submit">Save&nbsp;Settings</button>
                         </div>
                     </div>
                 </div>
@@ -147,10 +148,66 @@ export default {
   components:{},
   data () {
     return {
-      img:'',
+      id:'',
+      userData:{
+          username:'',
+          email:'',
+          password:'',
+          role: 'manufacturer',
+          gstNo:'',
+          address:'',
+          mobile:'',
+          description:'',
+          agreeTerms: true
+        },
     }
-  }
-}
+  },
+  async mounted(){
+    //   console.log(this.$strapi.user)
+      this.id = this.$strapi.user.id
+      this.userData.username=this.$strapi.user.username
+      this.userData.email=this.$strapi.user.email
+      this.userData.address=this.$strapi.user.address
+      this.userData.mobile=this.$strapi.user.mobile
+  },
+  methods:{
+      async saveUserSettings(){
+        try{
+            this.showLoading = true;
+            console.log("updating userData prcoess started...");
+            console.log(this.userData);
+        
+            const user = await this.$strapi.$users.update('me', this.userData)
+
+            this.$strapi.user = user
+
+            console.log("USER:",this.$strapi.user);
+        }
+        catch(e){
+            console.log("err:",e);
+            alert(e);
+            this.showLoading=false;
+        }
+      },
+      async saveContactSettings(){
+        try{
+            this.showLoading = true;
+            console.log("updating contactData prcoess started...");
+        
+            const user = await this.$strapi.$users.update('me', this.userData)
+
+            this.$strapi.user = user
+
+            console.log("USER:",this.$strapi.user);
+        }
+        catch(e){
+            console.log("err:",e);
+            alert(e);
+            this.showLoading=false;
+        }
+      },
+    }
+  };
 </script>
 
 <style lang="css" scoped>
