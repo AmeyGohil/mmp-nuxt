c9458452e92897f4dbd001ba58c8274236d7df80
Bhaskar Kishor <43471802+bhaskarkishor@users.noreply.github.com>
2021-08-17 12:52:57 +0530

    UPDATE: show details on order page

--- pages/customer/projects/_id.vue
+++ pages/customer/projects/_id.vue
@@ -3,7 +3,11 @@
     <div class="row">
       <div class="col-md-6">
         <div class="container-fluid">
-            <h3 class="text-dark mb-1">Project -{{$route.params.orderDetails.name}}</h3>
+            <h3 class="text-dark mb-1">Project -{{computed_order_details.name}}</h3>
+            <div v-for="(item,i) in computed_order_details_for_print" v-bind:key="i">
+              <p><b>{{item.key}}</b></p>
+              <p>{{item.value}}</p>
+            </div>
         </div>
       </div>
       <div class="col-md-6">
@@ -24,6 +28,20 @@ export default {
   data () {
     return {
       
+    }
+  },
+  computed:{
+    computed_order_details(){
+      return this.$route.params.orderDetails
+    },
+    computed_order_details_for_print(){
+      return Object.keys(this.computed_order_details)
+        .map(key => {
+          return {
+            key,
+            value: this.results[key] || 'n/a',
+          }
+        })
     }
   }
 }
